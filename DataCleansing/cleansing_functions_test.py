6cd339560ff15445ec6906e77c381edc1cf75202
ahasan-suncor <ahasan-suncor>
2023-02-24 19:07:17 +0000

    Add filter_priority_process_data func and tests

--- DataCleansing/cleansing_functions_test.py
+++ DataCleansing/cleansing_functions_test.py
@@ -3,7 +3,6 @@ from pyspark.sql.types import StructType, StructField, StringType
 import unittest
 
 class NormalizeColumnsTests(unittest.TestCase):
-    
     def test_normalize_column_names_with_data(self):
         data = [{'Product Category': 'A', 'ID': 1, 'Value': 121.44, 'some_column_': True}
               , {'Product Category': 'B', 'ID': 2, 'Value': 300.01, 'some_column_': False}
@@ -70,3 +69,84 @@ class RenameColumnsAfterAggTests(unittest.TestCase):
         actual_columns = normalized_df.columns
         expected_columns = expected_columns = ['column one', 'COLUMnTwO', 'column three', '']
         self.assertCountEqual(expected_columns, actual_columns)
+        
+class FilterPriorityProcessDataTests(unittest.TestCase):
+    def test_filter_priority_process_data_with_multiple_tags(self):
+        data = [{'timestamp': '2023-02-23 23:23:02', 'tag_name': 'tag_1', 'value': 121.44}
+              , {'timestamp': '2023-02-23 23:23:12', 'tag_name': 'tag_1', 'value': 121.44}
+              , {'timestamp': '2023-02-23 23:23:02', 'tag_name': 'tag_2', 'value': 121.44}
+              , {'timestamp': '2023-02-23 23:23:02', 'tag_name': 'tag_3', 'value': 121.44}
+              , {'timestamp': '2023-02-23 23:23:22', 'tag_name': 'tag_3', 'value': 121.44}
+              , {'timestamp': '2023-02-23 23:23:32', 'tag_name': 'tag_3', 'value': 121.44}
+              , {'timestamp': '2023-02-23 23:23:02', 'tag_name': 'tag_4', 'value': 121.44}
+               ]
+        spark_df_test = spark.createDataFrame(data)
+        priority_tags = ['tag_4', 'tag_2']
+        priority_tags_dict = {'tag_column_name': 'tag_name', 'priority_tags': priority_tags}
+        spark_df_filtered = filter_priority_process_data(spark_df_test, priority_tags_dict)
+        actual_tags_in_col = spark_df_filtered.select('tag_name').distinct()
+        expected_tags_in_col = priority_tags
+        self.assertTrue(expected_tags_in_col == expected_tags_in_col)
+        
+    def test_filter_priority_process_data_with_no_tags(self):
+        data = [{'timestamp': '2023-02-23 23:23:02', 'tag_name': 'tag_1', 'value': 121.44}
+              , {'timestamp': '2023-02-23 23:23:12', 'tag_name': 'tag_1', 'value': 121.44}
+              , {'timestamp': '2023-02-23 23:23:02', 'tag_name': 'tag_2', 'value': 121.44}
+              , {'timestamp': '2023-02-23 23:23:02', 'tag_name': 'tag_3', 'value': 121.44}
+              , {'timestamp': '2023-02-23 23:23:22', 'tag_name': 'tag_3', 'value': 121.44}
+              , {'timestamp': '2023-02-23 23:23:32', 'tag_name': 'tag_3', 'value': 121.44}
+              , {'timestamp': '2023-02-23 23:23:02', 'tag_name': 'tag_4', 'value': 121.44}
+               ]
+        spark_df_test = spark.createDataFrame(data)
+        priority_tags = []
+        priority_tags_dict = {'tag_column_name': 'tag_name', 'priority_tags': priority_tags}
+        spark_df_filtered = filter_priority_process_data(spark_df_test, priority_tags_dict)
+        actual_tags_in_col = spark_df_filtered.select('tag_name').distinct()
+        expected_tags_in_col = priority_tags
+        self.assertTrue(expected_tags_in_col == expected_tags_in_col)
+        
+    def test_filter_priority_process_data_with_no_tags_filtered(self):
+        data = [{'timestamp': '2023-02-23 23:23:02', 'tag_name': 'tag_1', 'value': 121.44}
+              , {'timestamp': '2023-02-23 23:23:12', 'tag_name': 'tag_1', 'value': 121.44}
+              , {'timestamp': '2023-02-23 23:23:02', 'tag_name': 'tag_2', 'value': 121.44}
+              , {'timestamp': '2023-02-23 23:23:02', 'tag_name': 'tag_3', 'value': 121.44}
+              , {'timestamp': '2023-02-23 23:23:22', 'tag_name': 'tag_3', 'value': 121.44}
+              , {'timestamp': '2023-02-23 23:23:32', 'tag_name': 'tag_3', 'value': 121.44}
+              , {'timestamp': '2023-02-23 23:23:02', 'tag_name': 'tag_4', 'value': 121.44}
+               ]
+        spark_df_test = spark.createDataFrame(data)
+        priority_tags = ['tag_1', 'tag_2', 'tag_3', 'tag_4']
+        priority_tags_dict = {'tag_column_name': 'tag_name', 'priority_tags': priority_tags}
+        spark_df_filtered = filter_priority_process_data(spark_df_test, priority_tags_dict)
+        actual_tags_in_col = spark_df_filtered.select('tag_name').distinct()
+        expected_tags_in_col = priority_tags
+        self.assertTrue(expected_tags_in_col == expected_tags_in_col)
+        
+    def test_filter_priority_process_data_with_some_tags_filtered(self):
+        data = [{'timestamp': '2023-02-23 23:23:02', 'tag_name': 'tag_1', 'value': 121.44}
+              , {'timestamp': '2023-02-23 23:23:12', 'tag_name': 'tag_1', 'value': 121.44}
+              , {'timestamp': '2023-02-23 23:23:02', 'tag_name': 'tag_2', 'value': 121.44}
+              , {'timestamp': '2023-02-23 23:23:02', 'tag_name': 'tag_3', 'value': 121.44}
+              , {'timestamp': '2023-02-23 23:23:22', 'tag_name': 'tag_3', 'value': 121.44}
+              , {'timestamp': '2023-02-23 23:23:32', 'tag_name': 'tag_3', 'value': 121.44}
+               ]
+        spark_df_test = spark.createDataFrame(data)
+        priority_tags = ['tag_1', 'tag_2', 'tag_3', 'tag_4']
+        priority_tags_dict = {'tag_column_name': 'tag_name', 'priority_tags': priority_tags}
+        spark_df_filtered = filter_priority_process_data(spark_df_test, priority_tags_dict)
+        actual_tags_in_col = spark_df_filtered.select('tag_name').distinct()
+        expected_tags_in_col = ['tag_1', 'tag_2', 'tag_3']
+        self.assertTrue(expected_tags_in_col == expected_tags_in_col)
+        
+    def test_filter_priority_process_data_with_no_data(self):
+        empty_schema = StructType([StructField('timestamp', StringType(), True)
+                                 , StructField('tag_name', StringType(), True)
+                                 , StructField('value', StringType(), True)
+                                 ])
+        spark_df_test = spark.createDataFrame([], schema = empty_schema)
+        priority_tags = ['tag_1', 'tag_2', 'tag_3', 'tag_4']
+        priority_tags_dict = {'tag_column_name': 'tag_name', 'priority_tags': priority_tags}
+        spark_df_filtered = filter_priority_process_data(spark_df_test, priority_tags_dict)
+        actual_tags_in_col = spark_df_filtered.select('tag_name').distinct()
+        expected_tags_in_col = []
+        self.assertTrue(expected_tags_in_col == expected_tags_in_col)
