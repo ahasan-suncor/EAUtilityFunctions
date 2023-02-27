9c0f9801381dba88ce06168686d43b4a56d97489
ahasan-suncor <ahasan-suncor>
2023-02-27 21:47:48 +0000

    Add pivot_precipitation_data function and ONE test

--- DataQuery/data_query_functions.py
+++ DataQuery/data_query_functions.py
@@ -24,3 +24,38 @@ def select_data_by_date_range(pandas_df: pd.DataFrame, date_range: List[str]) ->
 
     start_date, end_date = date_range
     return pandas_df.loc[start_date:end_date]
+
+# NOTE: !!!!!!!!THIS FUNCTION WAS RENAMED FROM "pivot_df" BECAUSE THATS NOT A ACCURATE AND DESCRIPTIVE NAME!!!!!
+def pivot_precipitation_data(pandas_df: pd.DataFrame, resample_frequency = 'M'
+          , date_column_name = 'DateTimeUTC', group_column_name: str = 'StationName'
+          , measure_column_name:str = 'PrecipAmountmm') -> pd.DataFrame:
+    """
+    Pivot precipitation data so that mean values of monthly precipitation is available for each city.
+
+    Args:
+        pandas_df: Input DataFrame with precipitation data.
+        date_column_name: Name of date column to use for pivoting.
+        group_column_name: Name of column to group by.
+        measure_column_name: Name of column containing precipitation amounts.
+        resample_frequency: Resampling frequency for the pivot table.
+                            Valid values: 'M' for monthly and 'D' for daily.
+
+    Returns:
+        A Pandas DataFrame pivoted with mean values of monthly precipitation for each city.
+
+    Assumptions:
+        The dataframe has index of type TimeStamp.
+    """
+
+    pandas_df_resampled = pandas_df[[group_column_name, measure_column_name]].groupby(group_column_name) \
+                                                                             .resample(resample_frequency) \
+                                                                             .mean()
+    
+    pandas_df_pivoted = pd.pivot_table(pandas_df_resampled
+                                     , values = measure_column_name
+                                     , index = pandas_df_resampled.index
+                                     , columns = group_column_name
+                                     , aggfunc = 'mean'
+                                     , fill_value = None)
+
+    return pandas_df_pivoted
--- DataQuery/data_query_functions_test.py
+++ DataQuery/data_query_functions_test.py
@@ -2,6 +2,7 @@
 import unittest
 import pandas as pd
 from datetime import datetime
+from pandas.testing import assert_series_equal
 
 class SelectDataByDateRangeTests(unittest.TestCase):
 
@@ -29,4 +30,34 @@ class SelectDataByDateRangeTests(unittest.TestCase):
         start_date = datetime(2022, 1, 4)
         end_date = datetime(2022, 1, 2)
         pandas_df_filtered_actual = select_data_by_date_range(self.pandas_df_test, [start_date, end_date])
-        self.assertTrue(pandas_df_filtered_actual.empty)
+        self.assertTrue(pandas_df_filtered_actual.empty)     
+
+class PivotPrecipitationDataTests(unittest.TestCase):
+    
+    def setUp(self):
+        self.pandas_df_test = pd.DataFrame({'DateTimeUTC': [datetime(2022, 1, 1, 12, 7, 21), datetime(2022, 1, 1, 13, 8, 21), datetime(2022, 1, 2, 15, 9, 21)
+                                              , datetime(2022, 1, 2, 18, 3, 21), datetime(2022, 2, 1, 19, 7, 21), datetime(2022, 2, 1, 19, 8, 21)]
+                              , 'StationName': ['PINCHER CREEK', 'FORT MCMURRAY CS', 'RED DEER REGIONAL A', 'EDMONTON BLATCHFORD', 'CALGARY INTL A', 'LETHBRIDGE A']
+                              , 'PrecipAmountmm': [10, 20, 15, 25, 30, 40]})
+        self.pandas_df_test.set_index('DateTimeUTC', inplace = True)
+        
+    def test_pivot_precipitation_data_monthly(self):
+        pandas_series_actual = pivot_precipitation_data(self.pandas_df_test, resample_frequency = 'M').mean()
+        pandas_series_actual = pandas_series_actual.reset_index(drop = True)
+        expected_data = {'CALGARY INTL A': 30.0
+                       , 'EDMONTON BLATCHFORD': 25.0
+                       , 'FORT MCMURRAY CS': 20.0
+                       , 'LETHBRIDGE A': 40.0
+                       , 'PINCHER CREEK': 10.0
+                       , 'RED DEER REGIONAL A': 15.0
+                        }
+        pandas_series_expected = pd.Series(expected_data)
+        pandas_series_expected = pandas_series_expected.reset_index(drop = True)
+                
+        assert_series_equal(pandas_series_actual, pandas_series_expected)
+        
+#     def test_pivot_precipitation_data_with_empty_df(self):
+#         pandas_df_empty = pd.DataFrame()
+#         pandas_df_actual_output = pivot_precipitation_data(pandas_df_empty)
+#         display(pandas_df_actual_output)
+#         self.assertTrue(pandas_df_actual_output.empty)
