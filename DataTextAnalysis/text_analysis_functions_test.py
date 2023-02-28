3bccaa19f6f2db62e7d866e86c84f476bea8cb9f
ahasan-suncor <ahasan-suncor>
2023-02-28 15:54:50 +0000

    Add remove_stopwords and lemmatize_text function and tests

---
+++ DataTextAnalysis/text_analysis_functions_test.py
@@ -0,0 +1,51 @@
+# Databricks notebook source
+import unittest
+
+class RemoveStopwordsTests(unittest.TestCase):
+
+    def test_remove_stopwords_sentence(self):
+        input_text = 'The quick brown fox jumps over the lazy dog.'
+        expected_output = 'quick brown fox jumps lazy dog.'
+        self.assertEqual(remove_stopwords(input_text), expected_output)
+        
+    def test_remove_stopwords_single_word(self):
+        input_text = 'Amazing'
+        expected_output = 'Amazing'
+        self.assertEqual(remove_stopwords(input_text), expected_output)
+        
+    def test_remove_stopwords_non_alpha_numeric(self):
+        input_text = '12 # @3456 $%^ ( &* !'
+        expected_output = '12 # @3456 $%^ ( &* !'
+        self.assertEqual(remove_stopwords(input_text), expected_output)
+
+    def test_remove_stopwords_empty_string(self):
+        input_text = ''
+        expected_output = ''
+        self.assertEqual(remove_stopwords(input_text), expected_output)
+
+    def test_remove_stopwords_stopword_only_input(self):
+        input_text = 'so what the an the and a'
+        expected_output = ''
+        self.assertEqual(remove_stopwords(input_text), expected_output)
+        
+class TestLemmatizeText(unittest.TestCase):
+
+    def test_lemmatize_text_with_punctuation(self):
+        input_text = 'am, are, is!'
+        expected_output = 'be'
+        self.assertEqual(lemmatize_text(input_text), expected_output)
+
+    def test_lemmatize_text_with_same_word(self):
+        input_text = "car, cars, car's, cars'"
+        expected_output = 'car'
+        self.assertEqual(lemmatize_text(input_text), expected_output)
+        
+    def test_lemmatize_text_sentence(self):
+        input_text = 'The striped bats are hanging on their feet for best.'
+        expected_output = 'The striped bat be hang on their foot for best'
+        self.assertEqual(lemmatize_text(input_text), expected_output)
+        
+    def test_lemmatize_text_multiple_words(self):
+        input_text = 'organize, organizes, and organizing'
+        expected_output = 'organize and'
+        self.assertEqual(lemmatize_text(input_text), expected_output)
