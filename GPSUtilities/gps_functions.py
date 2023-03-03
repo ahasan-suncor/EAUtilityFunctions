ad52b5e1ab1111bdcacdea79fbf5972fab84cee8
ahasan-suncor <ahasan-suncor>
2023-03-03 21:59:42 +0000

    Add convert_steepbank_xy_to_lon_lat function and test

--- GPSUtilities/gps_functions.py
+++ GPSUtilities/gps_functions.py
@@ -1,13 +1,14 @@
 # Databricks notebook source
 import numpy as np
+from typing import Dict
 
-def convert_steepbank_xy_to_utm(x: float, y: float, z: float = 100) -> dict:
+def convert_steepbank_xy_to_utm(x: float, y: float, z: float = 100) -> Dict[str, float]:
     """
     Converts Steepbank (m) x, y coordinates to UTM coordinates. This formula was provided by Suncor's GIS Team.
 
     Args:
-        x: x-coordinate of the point.
-        y: y-coordinate of the point.
+        x: x-coordinate in Steepbank system.
+        y: y-coordinate in Steepbank system.
 
     Returns:
         dict: A dictionary containing the easting, northing, and elevation coordinates.
@@ -55,6 +56,48 @@ def convert_steepbank_xy_to_utm(x: float, y: float, z: float = 100) -> dict:
     coords = {'easting': easting
             , 'northing': northing
             , 'elevation': elevation
+             }
+
+    return coords
+
+def convert_steepbank_xy_to_lon_lat(x: float, y: float) -> Dict[str, float]:
+    """
+    Converts Steepbank (m) x, y coordinates to longitude and latitude. This formula was provided by Suncor's GIS Team.
+    
+    Args:
+        x: x-coordinate in Steepbank system.
+        y: y-coordinate in Steepbank system.
+        
+    Returns:
+        dict: A dictionary containing the converted coordinates, with keys 'longitude' and 'latitude'.
+    """
+    Scale_in = 0.004321953
+    Scale_out = 0.003637212
+    X1 = 0.137462175
+    Xshift_in = -150169.25
+    Xshift_out = -111.394916
+    Xx = 1.044954407
+    Xx2 = 6.90E-08
+    Xxy = 5.81E-05
+    Xy = 4.05E-05
+    Xy2 = -6.83E-08
+    Y1 = 0.035543067
+    Yshift_in = -246627.855
+    Yshift_out = 56.93738889
+    Yx = -2.21E-05
+    Yx2 = -1.59E-05
+    Yxy = 7.63E-08
+    Yy = 0.571219926
+    Yy2 = -9.54E-08
+
+    x = (x + Xshift_in) * Scale_in
+    y = (y + Yshift_in) * Scale_in
+
+    x_p = Xx * x + Xy * y + X1 + Xxy * x * y + Xx2 * x**2 + Xy2 * y**2
+    y_p = Yx * x + Yy * y + Y1 + Yxy * x * y + Yx2 * x**2 + Yy2 * y**2
+
+    coords = {'longitude': x_p * Scale_out + Xshift_out
+            , 'latitude': y_p * Scale_out + Yshift_out
              }
 
     return coords
--- GPSUtilities/gps_functions_test.py
+++ GPSUtilities/gps_functions_test.py
@@ -13,3 +13,16 @@ class ConvertSteepbankXYToUTMTests(unittest.TestCase):
 
         self.assertAlmostEqual(xy_converted_utm['easting'], expected_easting, delta = 0.0001)
         self.assertAlmostEqual(xy_converted_utm['northing'], expected_northing, delta = 0.0001)
+
+class ConvertSteepbankXYToLonLatTests(unittest.TestCase):
+
+    def test_convert_steepbank_xy_to_lon_lat(self):
+        x = 148575.75
+        y  = 254252.32
+        xy_converted_lon_lat = convert_steepbank_xy_to_lon_lat(x, y)
+
+        expected_longitude = -111.4206354
+        expected_latitude = 57.0059799
+
+        self.assertAlmostEqual(xy_converted_lon_lat['longitude'], expected_longitude, delta = 0.0001)
+        self.assertAlmostEqual(xy_converted_lon_lat['latitude'], expected_latitude, delta = 0.0001)
