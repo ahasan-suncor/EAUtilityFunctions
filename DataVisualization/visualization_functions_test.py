2db7ea8da5b068276f4f7f42ee0f2c69efb0383d
david-hurley <hurleyldave@gmail.com>
2023-03-08 08:41:30 -0700

    added test for stacked chart

--- DataVisualization/visualization_functions_test.py
+++ DataVisualization/visualization_functions_test.py
@@ -157,7 +157,44 @@ class test_plotting_functions(unittest.TestCase):
         self.assertEqual(fig.to_ordered_dict()['layout']['title']['text'], title)
         self.assertEqual((fig.to_ordered_dict()['layout']['width'], fig.to_ordered_dict()['layout']['height']), size)
 
+        col_names_output = [fig.to_ordered_dict()['data'][0]['dimensions'][0]['label'], fig.to_ordered_dict()['data'][0]['dimensions'][1]['label'], fig.to_ordered_dict()['data'][0]['dimensions'][2]['label']]
+        self.assertListEqual(col_names_output, col_names)
 
-    # def test_cluster_stack_barplot(self):
+    def test_cluster_stack_barplot(self):
+        
+        # input data
+        df = pd.DataFrame({
+            'StationName': ['CALGARY INTL A', 'CALGARY INTL A', 'CALGARY INTL A', 'CALGARY INTL A'],
+            'Month': [1, 2, 1, 2],
+            'Cnt': [10, 9, 30, 40],
+            'DayType': ['AboveZeroDays', 'AboveZeroDays', 'BelowZeroDays', 'BelowZeroDays']
+            })
+        
+        # layout settings
+        x_axis_variable='StationName'
+        y_axis_variable='Cnt'
+        group_by_variable='DayType'
+        stack_by_variable='Month'
+        y_axis_name='Count'
+        x_axis_name='Station Name'
+        title='Weather Data Grouped for Above and Below Zero Days'
+        size=(1000, 800)
+        
+        fig = my_plt.cluster_stack_barplot(pandas_df=df, 
+                                           x_axis_variable=x_axis_variable, 
+                                           y_axis_variable=y_axis_variable, 
+                                           group_by_variable=group_by_variable, 
+                                           stack_by_variable=stack_by_variable,
+                                           y_axis_name=y_axis_name, 
+                                           x_axis_name=x_axis_name,
+                                           title=title,
+                                           size=size)
+
+        # test layout of figure object
+        self.assertEqual(fig.to_ordered_dict()['layout']['title']['text'], title)
+        self.assertEqual((fig.to_ordered_dict()['layout']['width'], fig.to_ordered_dict()['layout']['height']), size)
+        self.assertEqual(fig.to_ordered_dict()['layout']['xaxis']['title']['text'], x_axis_name)
+        self.assertEqual(fig.to_ordered_dict()['layout']['yaxis']['title']['text'], y_axis_name)
+
 
 unittest.main(argv=[''], verbosity=2, exit=False)
